a36a3f27a728cbf4eb355dae77b9cda8af76d80a
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-23 21:40:03 -0300

    req res

--- Aula Node/Primeiro Servidor/server.js
+++ Aula Node/Primeiro Servidor/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("olá!");
+});
+
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
