a063c831772359aecc398dd318a4af88052d81ae
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-25 19:45:35 -0300

    encontrando usuários

--- Aula Node/Primeiro Servidor/server.js
+++ Aula Node/Primeiro Servidor/server.js
@@ -2,6 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const usuarios = [
+    //objetos no formato JSON
+    {id: 1, nome: "Nicolas", idade: 20},
+    {id: 2, nome: "Gabriel", idade: 20}
+];
+
+//Get all - buscar todos os usuários 
+app.get("/usuarios", (req, res) => {
+    res.send(usuarios);
+});
+
+//get by id - buscar usuário por id
+app.get("/usuarios/:id", (req, res) => {
+    //pegar o id na requisição
+    const id = parseInt(req.params.id);
+    //usa o find para encontrar o usuário pelo id
+    const usuario = usuarios.find(u => u.id === id);
+    if(!usuario){
+        res.status(404).send("Usuário não encontrado!");
+    }
+    res.status(200).send(usuario);
+});
+
+//opcional
 app.get("/", (req, res) => {
     res.send("olá!");
 });
