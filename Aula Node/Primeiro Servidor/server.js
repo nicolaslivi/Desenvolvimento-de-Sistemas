a35ee61904b745f1a9c78436cea8de8224998444
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-25 21:42:23 -0300

    deletando usuario

--- Aula Node/Primeiro Servidor/server.js
+++ Aula Node/Primeiro Servidor/server.js
@@ -46,6 +46,40 @@ app.post("/usuarios", (req, res) => {
     res.status(201).send(novoUsuario); // mostra o novo usuário e da o status de "criado com sucesso"
 });
 
+//Put - atualizar usuário
+app.put("/usuarios/:id", (req, res) => {
+    let novosDados = req.body;
+    const id = parseInt(req.params.id);
+
+    if (!novosDados.nome || novosDados.idade <= 0){
+        res.status(400).send("E necessario preencher idade e nome!");
+    };
+
+    novosDados.id = id;
+    const index = usuarios.findIndex(u => u.id === id);
+
+    if(!index){
+        res.status(404).send("Usuario nao encontrado!");
+    };
+
+    usuarios[index] = novosDados;
+    res.status(204).send();
+
+});
+
+//Delete - remover usuario
+app.delete("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = usuarios.findIndex(u => u.id === id);
+
+    if(index == -1){
+        res.status(404).send("Usuario nao encontrado!");
+    };
+    
+    usuarios.splice(index, 1);
+    res.status(204).send();
+});
+
 //opcional
 app.get("/", (req, res) => {
     res.send("olá!");
