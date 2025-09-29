cd6d69b4918b9c9524d96096f7752b952c958430
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-29 20:14:23 -0300

    exercicio 1

--- Aula Node/Lista de exercicios/Exercicio 1/server1.js
+++ Aula Node/Lista de exercicios/Exercicio 1/server1.js
@@ -12,7 +12,35 @@ let tarefas = [
     {id: 2, titulo: 'Fazer exercícios', concluida: false}
 ];
 
+//novo ID
+let nextId = 3;
 
+//retorna todas as tarefas existentes
+app.get('/tarefas', (req, res) =>{
+    res.send(tarefas);
+});
+
+//retorna as tarefas com base no seu ID
+app.get('/tarefas/:id', (req, res) => {
+    //pega o id desejado
+    let id = parseInt(req.params.id);
+    //pega uma das tarefas no array usando o find
+    //'t' é a minha variável para tarefas
+    let tarefa = tarefas.find(t => t.id === id);
+    //verifica se a tarefa selecionada existe
+    if(!tarefa){
+        //status 404: não encontrado
+        res.status(404).send('Tarefa não encontrada!');
+    }
+    //retorna a tarefa selecionada pelo ID
+    //status 200: OK
+    res.status(200).send(tarefa);
+});
+
+//cria uma nova tarefa
+app.post('/tarefas', (req, res) => {
+    
+});
 
 //aparece no terminal com o link para abrir o servidor na web
 app.listen(port, () => {
