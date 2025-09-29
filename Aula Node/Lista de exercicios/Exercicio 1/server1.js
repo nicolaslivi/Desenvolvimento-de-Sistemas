6633b54b18bac5d2249def0052f2a222fd82b50d
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-29 19:15:38 -0300

    começando exercício 1

---
+++ Aula Node/Lista de exercicios/Exercicio 1/server1.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+//usa o formato json nas reqs - middleware
+app.use(express.json());
+
+//aqui vai o exercício
+
+
+//aparece no terminal com o link para abrir o servidor na web
+app.listen(port, () => {
+    console.log(`O servidor está rodando em http://localhost:${port}`);
+});
