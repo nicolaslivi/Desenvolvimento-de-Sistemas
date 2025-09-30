ba60614f79ff6fb8104b17226e29cb3d926e940c
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-30 20:12:51 -0300

    parte ex 2

--- Aula Node/Lista de exercicios/Exercicio 2/server2.js
+++ Aula Node/Lista de exercicios/Exercicio 2/server2.js
@@ -13,18 +13,36 @@ let produtos = [
     {id: 3, nome: 'Monitor ultrawide', preco: 1500.00, emEstoque: true}
 ];
 
-// novo id a partir dos de cima
+// novo id a partir dos outros
 let nextId = 4;
 
 //retorna os produtos em estoque
 app.get('/produtos/em-estoque', (req, res) => {
     //recebe o parâmetro emEstoque do array produtos na variável estoque
-    let estoque = (req.params.emEstoque);
+    let estoque = [];
     //verifica quais os produtos em estoque
-    if(estoque = true){
-        
+    for(let x=0; x < produtos.length; x++){
+        //passa pelos itens do array e verifica qual tem em estoque
+        if(produtos[x].emEstoque === true){
+            //pega os produtos em estoque e adiciona em um novo vetor 'estoque'
+            estoque.push(produtos[x]);
+        }
     }
+    //mostra os itens em estoque
+    res.send(estoque);
 });
+
+//pesquisa os produtos pelo nome
+app.get('/produtos/pesquisar', (req, res) => {
+    let nome = req.query.nome
+    if(!nome){
+        res.status(404).send('Nome nao encontrado!');
+    }
+
+    let nomeEncontrado = produtos.filter(n => n.nome.toLowerCase().includes(nome.toLowerCase()));
+
+    console.log(nomeEncontrado);
+}); 
 
 //aparece no terminal com o link para abrir o servidor na web
 app.listen(port, () => {
