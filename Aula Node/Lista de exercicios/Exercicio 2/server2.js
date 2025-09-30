d4f4965e1a9617935d599522a2432f17e0f10782
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-29 21:42:26 -0300

    aula 29/09

--- Aula Node/Lista de exercicios/Exercicio 2/server2.js
+++ Aula Node/Lista de exercicios/Exercicio 2/server2.js
@@ -9,10 +9,22 @@ app.use(express.json());
 //array de produtos
 let produtos = [
     {id: 1, nome: 'Teclado mecanico', preco: 350.00, emEstoque: true},
-    {id: 1, nome: 'Mouse gamer', preco: 180.00, emEstoque: false},
-    {id: 1, nome: 'Monitor ultrawide', preco: 1500.00, emEstoque: true}
+    {id: 2, nome: 'Mouse gamer', preco: 180.00, emEstoque: false},
+    {id: 3, nome: 'Monitor ultrawide', preco: 1500.00, emEstoque: true}
 ];
 
+// novo id a partir dos de cima
+let nextId = 4;
+
+//retorna os produtos em estoque
+app.get('/produtos/em-estoque', (req, res) => {
+    //recebe o parâmetro emEstoque do array produtos na variável estoque
+    let estoque = (req.params.emEstoque);
+    //verifica quais os produtos em estoque
+    if(estoque = true){
+        
+    }
+});
 
 //aparece no terminal com o link para abrir o servidor na web
 app.listen(port, () => {
