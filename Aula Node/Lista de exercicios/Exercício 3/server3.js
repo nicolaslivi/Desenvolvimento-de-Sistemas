17d2c48dfd33b37292ee37a19c0f5a43eb92791a
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-07 19:08:10 -0300

    colocando express no server3

--- Aula Node/Lista de exercicios/Exercício 3/server3.js
+++ Aula Node/Lista de exercicios/Exercício 3/server3.js
@@ -6,7 +6,23 @@ const port = 3003;
 app.use(express.json());
 
 //aqui vai o exercício
+let posts = [
+    { id: 1, titulo: 'Primeiro Post', conteudo: 'ola', autor: 'Nicolas'},
+    { id: 2, titulo: 'Segundo Post', conteudo: 'tchau', autor: 'Julia'}
+];
 
+//novo ID a ser incrementado
+let nextId = 3;
+
+//criando um novo post
+app.post('/posts', (req, res) => {
+    //recebendo novo post no body
+    let novoPost = req.body;
+    //verificando se os conteudos a serem adicionados existem e estão preenchidos
+    if(!novoPost.titulo || !novoPost.conteudo || !novoPost.autor){
+        res.status(400).send('Necessario informar titulo, conteudo e autor para criar um post!');//status 400- bad request (má requisição)
+    }
+});
 
 //aparece no terminal com o link para abrir o servidor na web
 app.listen(port, () => {
