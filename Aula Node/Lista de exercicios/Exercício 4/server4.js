d89ca8aa7d3de7e6007f53aee0ceeb9ff1be1dd3
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-07 21:02:41 -0300

    começando exercicio 4

--- Aula Node/Lista de exercicios/Exercício 4/server4.js
+++ Aula Node/Lista de exercicios/Exercício 4/server4.js
@@ -6,7 +6,16 @@ const port = 3004;
 app.use(express.json());
 
 //aqui vai o exercício
+let comentarios = [
+    { id: 1, post_id: 1, texto: "topzera!"},
+    { id: 2, post_id: 2, texto: "bacana!"}
+];
 
+//listar comentários de um post
+app.get('/posts/:id/comentarios', (req, res) => {
+    let id = parseInt(req.params.id);
+    
+});
 
 //aparece no terminal com o link para abrir o servidor na web
 app.listen(port, () => {
