45fde3ede3be2ce9077afc29bce030d8f01156c4
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-09 20:17:33 -0300

    exercico 4 finalizado

--- Aula Node/Lista de exercicios/Exercício 4/server4.js
+++ Aula Node/Lista de exercicios/Exercício 4/server4.js
@@ -11,10 +11,43 @@ let comentarios = [
     { id: 2, post_id: 2, texto: "bacana!"}
 ];
 
+let nextId = 3;
+
 //listar comentários de um post
 app.get('/posts/:id/comentarios', (req, res) => {
     let id = parseInt(req.params.id);
-    
+    const index = comentarios.findIndex(c => c.post_id === id);
+     //verifico se o id é válido
+     if(index == -1){
+        res.status(404).send('Post nao encontrado!');//404- not found
+    }
+    res.status(200).send(comentarios[index].texto);
+});
+
+//cria um novo comentário em um post específico
+app.post('/posts/:id/comentarios', (req, res) => {
+    let id = parseInt(req.params.id);
+     //verifico se o id é válido
+     const index = comentarios.findIndex(c => c.post_id === id);
+     if(index == -1){
+        res.status(404).send('Post nao encontrado!');//404- not found
+    }
+    let novoComentario = req.body;
+    novoComentario.post_id = id;
+    novoComentario.id = nextId++;
+    comentarios.push(novoComentario);
+    res.status(201).send(comentarios);
+});
+
+//remove um comentário
+app.delete('/comentarios/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    const index = comentarios.findIndex(c => c.id === id);
+    if(index == -1){
+        res.status(404).send('Comentario nao encontrado!');//404- not found
+    }
+    comentarios.splice(index, 1);
+    res.status(200).send(comentarios);
 });
 
 //aparece no terminal com o link para abrir o servidor na web
