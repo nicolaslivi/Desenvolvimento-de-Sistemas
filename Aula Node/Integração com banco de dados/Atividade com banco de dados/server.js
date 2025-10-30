5696a95a330126048a170dc1ac22a6899d73d3b0
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-30 20:44:06 -0300

    testando

--- Aula Node/Integração com banco de dados/Atividade com banco de dados/server.js
+++ Aula Node/Integração com banco de dados/Atividade com banco de dados/server.js
@@ -7,6 +7,15 @@ app.use(express.json());
 
 const db = require('./db');
 
-
+//1- Tarefas: busca todas as tarefas
+app.get('/tarefas', async (req, res) => {
+    try{
+        const [rows] = await db.query('SELECT * FROM aula_api_db.tarefas');
+        res.json(rows);
+    } catch(error) {
+        console.error('Erro ao buscar tarefas:', error);
+        res.status(500).send('Erro interno do servidor ao buscar tarefas.');
+    }
+});
 
 app.listen(port, () => console.log(`Rodando aqui http://localhost:${port}`));
