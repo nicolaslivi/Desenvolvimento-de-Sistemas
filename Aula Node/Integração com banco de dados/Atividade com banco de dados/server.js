ac55fe567ceb16080c9686910de9ddccdcf90b0f
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-30 20:57:07 -0300

    criando tarefa

--- Aula Node/Integração com banco de dados/Atividade com banco de dados/server.js
+++ Aula Node/Integração com banco de dados/Atividade com banco de dados/server.js
@@ -18,4 +18,30 @@ app.get('/tarefas', async (req, res) => {
     }
 });
 
+//1- Tarefas: coloca uma nova tarefa
+app.post('/tarefas', async (req, res) => {
+    const { titulo, descricao, concluida } = req.body;
+  
+    if (!titulo) {
+      return res.status(400).send('O título da tarefa é obrigatório.');
+    } else if (!descricao) {
+        return res.status(400).send('A descrição da tarefa é obrigatória.');
+    } else if (!concluida) {
+        return res.status(400).send('O status da tarefa é obrigatório.');
+    }
+  
+    try {
+      const [result] = await db.query(
+        'INSERT INTO tarefas (titulo, descricao, concluida) VALUES (?, ?, ?)',
+        [titulo, descricao || null, concluida || false]
+      );
+  
+      const novaTarefa = { id: result.insertId, titulo, descricao, concluida };
+      res.status(201).json(novaTarefa);
+    } catch (error) {
+      console.error('Erro ao criar tarefa:', error);
+      res.status(500).send('Erro interno do servidor ao criar tarefa.');
+    }
+  });
+
 app.listen(port, () => console.log(`Rodando aqui http://localhost:${port}`));
